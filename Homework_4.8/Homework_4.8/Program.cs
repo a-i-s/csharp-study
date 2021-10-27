202f417014a6e1dee61460049fc96c33dd259cec
Anastasiya Surkova <surkova.a.i@list.ru>
2021-10-27 09:01:07 +0200

    изменено задание 4.8. Первая рабочая версия.

--- Homework_4.8/Homework_4.8/Program.cs
+++ Homework_4.8/Homework_4.8/Program.cs
@@ -11,38 +11,93 @@ namespace Homework_4._8
     {
         static void Main(string[] args)
        {
-            /* int income;
-             int expense;
-             int profit;
-
-             Random rand = new Random();
-
-             Console.WriteLine($"{"месяц",16}   {"доход,тыс.руб",16}   {"расход,тыс.руб",16}   {"прибыль,тыс.руб.",16}");
-             for (int i = 1; i < 13; i++)
-             {
-                 income = rand.Next(1, 100);
-                 expense = rand.Next(1, 100);
-                 profit = income - expense;
-
-                 Console.WriteLine($"{i,16}   {income,16}   {expense,16}   {profit,16}");
-             }
-             Console.ReadKey();*/
-
-            int[] incomeArray = { 100000, 120000, 80000, 70000, 100000, 200000, 130000, 150000, 190000, 110000, 150000, 100000 };
+            int[] incomeArray = { 100000, 120000, 180000, 70000, 100000, 200000, 130000, 150000, 190000, 110000, 150000, 100000 };
             int[] expenseArray = { 80000, 90000, 70000, 70000, 80000, 120000, 140000, 65000, 90000, 70000, 120000, 80000 };
 
             int[] profitArray = new int[12];
 
             Console.WriteLine($"{"месяц",16}   {"доход,тыс.руб",16}   {"расход,тыс.руб",16}   {"прибыль,тыс.руб.",16}");
-            for (int i = 0; i < 12; i++)
+            for (int i = 0; i < 12; i++) // находим прибыль в каждом месяцце
              {
               profitArray[i] = incomeArray[i] - expenseArray[i];
               Console.WriteLine($"{i+1,16}   {incomeArray[i],16}   {expenseArray[i],16}   {profitArray[i],16}");
              }
             Console.ReadKey();
-        }
 
+            int counter = 0;
+            for (int i = 0; i < profitArray.Length; i++)
+            {
+                if (profitArray[i] > 0) counter++; // ищем положительную прибыль
+            }
+            Console.WriteLine($"Месяцев с положительной прибылью: {counter}");
+            Console.ReadKey();
+
+            int[] minProfitArray = new int[3];
+            for (int j = 0; j < 3; j++)
+            {
+               
+                int min = profitArray[0]; // находим минимальное значение прибыли
+                                             
+                               
+                for (int i = 1; i < profitArray.Length; i++)
+                {
+                                                                     
+                    int current = profitArray[i]; 
+                    if (current < min) {
+
+                       bool isFound = false; // найдено ли такое минимальное значение в массиве минимальных значений
+                    
+                   
+                       if (j>0) {
+                           for (int k = 0; k < j; k++)
+                           {
+                            if (minProfitArray[k] == current) { // если текущее значение уже есть в 
+                                isFound = true;                 // массиве минимальных значений, мы запоминаем
+                                break;                          // факт, что оно найдено и выходлим
+
+                                }
+                            }
+
+                        }
+                       
+                        if (!isFound) min = current;
+                    }
+    
+                }
+
+                minProfitArray[j] = min; 
+                Console.WriteLine($"Худшая прибыль в месяцах: {min} + {j} ");
+         
+            }
+
+            // далее находим месяцы с минимальной прибылью
+            string months = "";
+            for (int t = 0; t < minProfitArray.Length; t++)
+                {
+                 int number = minProfitArray[t]; // запомнили элемент из массива минимальных значений
+                
+                for (int g = 0; g < profitArray.Length; g++)
+                {
+                    int number2 = profitArray[g]; // запомнили элемент из массива значений прибыли
+                    if (number == number2) {
+                      months = months + (g+1) + " ";
+                      Console.WriteLine(g+1);
+                     }
+                    
+                }
+                                                                                                   
+             Console.WriteLine($"Худшая прибыль в месяцах: {months}");
+           }
+        Console.ReadKey();
+         /*int min = profitArray[0];
+            for (int i = 1; i < profitArray.Length; i++)
+            {
+                int current = profitArray[i];
+                if (profitArray[i] < profitArray[0]) min = profitArray[i];
+            }*/
+         
     }
 
+}
 }
 
