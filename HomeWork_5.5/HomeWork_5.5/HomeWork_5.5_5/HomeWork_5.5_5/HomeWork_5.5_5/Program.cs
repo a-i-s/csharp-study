b7055e4a4b14632ae683275a2e4378f9ec3791f7
Anastasiya Surkova <surkova.a.i@list.ru>
2021-11-23 09:38:54 +0200

    Метод рекурсии для вычисления функции Аккермана

---
+++ HomeWork_5.5/HomeWork_5.5/HomeWork_5.5_5/HomeWork_5.5_5/HomeWork_5.5_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork_5._5_5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberM = 2;
+            int numberN = 2;
+            Console.WriteLine(Recursion(numberM, numberN));
+        }
+
+        /// <summary>
+        /// метод рекурсии для вычисления функции Аккермана
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        static int Recursion(int numberM, int numberN) // m >= 0, n >= 0
+        {
+            if (numberM == 0) return numberN + 1;
+
+            // m >= 0, n > 0
+            if (numberN == 0) return Recursion(numberM - 1, 1);
+
+            // m > 0, n > 0
+            return Recursion(numberM - 1, Recursion(numberM, numberN - 1));
+        }
+    }
+}
